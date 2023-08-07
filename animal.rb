198a52c9c4455322c859700e1a277d24dcf775ea
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-07 14:16:31 +0300

    Include getters and setters for the methods

--- animal.rb
+++ animal.rb
@@ -1,8 +1,26 @@
 class Animal
+
   def initialize(type, number_of_legs, name = "unknown")
     @id = Random.rand(1..1000)
     @number_of_legs = number_of_legs
     @name = name
     @type = type
   end
+
+  def id
+    @id
+  end
+   
+  def number_of_legs
+    @number_of_legs
+  end
+
+  def type
+    @type
+  end
+
+  def name=(value)
+    @name = value
+  end 
+
 end
