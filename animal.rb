c250ec32f2f868c9a95509b549b657e78e7f2a01
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-09 11:36:52 +0300

    Fix linters

--- animal.rb
+++ animal.rb
@@ -1,5 +1,5 @@
-require './remover.rb'
-require './foods.rb'
+require './remover'
+require './foods'
 
 class Animal
   def initialize(type, number_of_legs, name = 'unknown')
@@ -7,7 +7,7 @@ class Animal
     @number_of_legs = number_of_legs
     @name = name
     @type = type
-    @liked_food = NoFood.new()
+    @liked_food = NoFood.new
   end
 
   attr_accessor :name
@@ -18,11 +18,11 @@ class Animal
   end
 
   def remove_leg
-    remover = Remover.new()
+    remover = Remover.new
     @number_of_legs = remover.decrease(@number_of_legs)
   end
 
   def likes_food?(food)
-    @liked_food.is_liked?(food)
+    @liked_food.liked?(food)
   end
 end
