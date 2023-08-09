197d33c0fce3ca8532bd2d0d1f5bc6b212a1f5d1
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-09 11:28:46 +0300

    Compose classes

--- animal.rb
+++ animal.rb
@@ -1,9 +1,13 @@
+require './remover.rb'
+require 'foods.rb'
+
 class Animal
   def initialize(type, number_of_legs, name = 'unknown')
     @id = Random.rand(1..1000)
     @number_of_legs = number_of_legs
     @name = name
     @type = type
+    @liked_food = NoFood.new()
   end
 
   attr_accessor :name
@@ -12,4 +16,12 @@ class Animal
   def speak
     'grrrr'
   end
+
+  def remove_leg
+    remover = Remover.new()
+    @number_of_legs = remover.decrease(@number_of_legs)
+  end
+
+  def likes_food?(food)
+    @liked_food.is_liked?(food)
 end
